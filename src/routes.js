d13c81e73169885de89f66a42687e5925f12f60b
Alvaro E <alvaro@bridge.ufsc.br>
2020-08-07 16:22:24 -0300

    links mechanism

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 import { Router } from 'express';
-
+const links = ['link1', 'link2'];
+let linkIndex = 1;
 const routes = new Router();
-routes.get('/', (req, res) => res.json({ message: 'hello' }));
+routes.get('/', (req, res) => {
+	linkIndex = linkIndex + 1 > links.length ? 0 : linkIndex + 1;
+	res.json({
+		link: links[linkIndex],
+	});
+});
 
 export default routes;
