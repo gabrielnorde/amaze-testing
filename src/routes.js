cc01a429603634eca9607af62fbc3695b571575e
Alvaro E <alvaro@bridge.ufsc.br>
2020-08-11 11:15:10 -0300

    links maze update

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-const links = ['link1', 'link2'];
+const links = [
+	'https://t.maze.design/15615430?guerilla=true',
+	'https://t.maze.design/15756155?guerilla=true',
+];
 let linkIndex = 1;
 const routes = new Router();
 routes.get('/', (req, res) => {
 	linkIndex = linkIndex + 1 > links.length - 1 ? 0 : linkIndex + 1;
-	res.json({
-		link: links[linkIndex],
-	});
+	res.status(301).redirect(links[linkIndex]);
 });
 
 export default routes;
